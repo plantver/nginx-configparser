1e3911800b9bd21c9a5c4bdc33c1fc34755d4839
Xingjian Yan <plantver@Xingjians-MacBook.local>
2017-01-17 18:16:12 -0800

    add some tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,29 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+// test NginxConfigStatement ToString
+TEST(NginxCongigTest, ToString){
+  NginxConfigStatement statement;
+  statement.tokens_.push_back("foo");
+  statement.tokens_.push_back("bar");
+  EXPECT_EQ(statement.ToString(0), "foo bar;\n");
+}
 
+// Fixture
+class NginxConfigParserTest : public ::testing::Test {
+  protected:
+    NginxConfigParser parser;
+    NginxConfig out_config;
+};
+
+TEST_F(NginxConfigParserTest, SimpleConfig) {
   bool success = parser.Parse("example_config", &out_config);
-
   EXPECT_TRUE(success);
 }
+
+TEST_F(NginxConfigParserTest, foobar){
+  std::string config_string = "foo { bar; ";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
+
