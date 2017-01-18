69f84d53e207f7fd8f69aa870d6a807a9557857a
Xingjian Yan <yxjnb@g.ucla.edu>
2017-01-17 19:19:30 -0800

    more unit testing

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -21,9 +21,58 @@ TEST_F(NginxConfigParserTest, SimpleConfig) {
   EXPECT_TRUE(success);
 }
 
+// Known bug
 TEST_F(NginxConfigParserTest, missMatchBrakets){
   std::string config_string = "foo { bar; ";
   std::stringstream config_stream(config_string);
   EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
 }
 
+TEST_F(NginxConfigParserTest, missingSemicolon){
+  std::string config_string = "foo bar ";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
+
+TEST_F(NginxConfigParserTest, multiStatement){
+  std::string config_string = "foo; bar;";
+  std::stringstream config_stream(config_string);
+  EXPECT_TRUE(parser.Parse(&config_stream, &out_config));
+}
+
+TEST_F(NginxConfigParserTest, noToken ){
+  std::string config_string = ";";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
+
+TEST_F(NginxConfigParserTest, noTokenRecursive ){
+  std::string config_string = "a {;}";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
+
+TEST_F(NginxConfigParserTest, noSemicolonRecursive ){
+  std::string config_string = "a {b}";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
+
+TEST_F(NginxConfigParserTest, configAfterBracket ){
+  std::string config_string = "a {b;} c;";
+  std::stringstream config_stream(config_string);
+  EXPECT_TRUE(parser.Parse(&config_stream, &out_config));
+}
+
+TEST_F(NginxConfigParserTest, manyStatementOneLine ){
+  std::string config_string = "a {b; d; e; f;} c; a b c {d; t;}";
+  std::stringstream config_stream(config_string);
+  EXPECT_TRUE(parser.Parse(&config_stream, &out_config));
+}
+
+// Should empty config be allowed in brackets?
+TEST_F(NginxConfigParserTest, emptyConfig ){
+  std::string config_string = "a {}";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
