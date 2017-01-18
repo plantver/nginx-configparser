bf4e85565533da57569b0c316e2748f417bcfff0
Chang Liu <liuchang2872@ucla.edu>
2017-01-17 19:28:24 -0800

    adding test instruction

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,11 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+
+//Edit config_parser_test.cc in your repository.
+// You will note the test file already has a passing example. At least one unit test should fail due to bugs in the code you are testing.
+// Don't modify config_parser.cc until you identify the bugs -- it is ALREADY buggy.
+// Also, there is probably no need to review the code you're testing -- just write unit tests until you find a failure.
+// You may be able to find some Nginx config docs or web pages on the internet.
+
